69acc46bf1175082bb436d7dd0e5324e7f90ac87
Vincent <vincent.desmares@gmail.com>
2023-08-07 15:32:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,39 +45,18 @@ resource "aws_security_group_rule" "bastion_all_egress" {
   security_group_id = aws_security_group.bastion.id
 }
 
-data "template_file" "user_data" {
-  template = var.user_data_file != "" ? var.user_data_file : file("${path.module}/user_data.sh")
-
-  vars = {
+resource "aws_launch_configuration" "bastion" {
+  name_prefix       = "${var.name}-"
+  image_id          = var.ami
+  instance_type     = var.instance_type
+  user_data         = templatefile(var.user_data_file != "" ? var.user_data_file : "${path.module}/user_data.sh",{
     s3_bucket_name              = var.s3_bucket_name
     s3_bucket_uri               = var.s3_bucket_uri
     ssh_user                    = var.ssh_user
     keys_update_frequency       = var.keys_update_frequency
     enable_hourly_cron_updates  = var.enable_hourly_cron_updates
     additional_user_data_script = var.additional_user_data_script
-  }
-}
-
-//resource "aws_instance" "bastion" {
-//  ami                    = "${var.ami}"
-//  instance_type          = "${var.instance_type}"
-//  iam_instance_profile   = "${var.iam_instance_profile}"
-//  subnet_id              = "${var.subnet_id}"
-//  vpc_security_group_ids = ["${aws_security_group.bastion.id}"]
-//  user_data              = "${template_file.user_data.rendered}"
-//
-//  count                  = 1
-//
-//  tags {
-//    Name = "${var.name}"
-//  }
-//}
-
-resource "aws_launch_configuration" "bastion" {
-  name_prefix       = "${var.name}-"
-  image_id          = var.ami
-  instance_type     = var.instance_type
-  user_data         = data.template_file.user_data.rendered
+  })
   enable_monitoring = var.enable_monitoring
 
   security_groups = compact(
